a92037cd7b40d5577f304f4f9d28e6136f48464f
Tjernell <ericat@telia.com>
2025-02-20 15:47:32 +0100

    started on importance/brdf sampling

--- test.py
+++ test.py
@@ -5,13 +5,13 @@ import csv
 import locale
 if __name__ == '__main__':
     
-    # ref = "dump/Sponza_KiaraDawn_Main_ReferencePathTracer_3288_0.005070.png"
+    ref = "dump/ref/BreakfastRoom_KiaraDawn_Main_None_ReferencePathTracer_2817_0.010464.png"
 
 
-    # test = "dump/Sponza_KiaraDawn_Main_GI-1.1_328_0.005492.png"
+    test = "dump/test/BreakfastRoom_KiaraDawn_Main_None_GI-1.1_6107_0.004189.png"
     
-    # flipErrorMap, meanFLIPError, parameters = flip.evaluate(ref, test, "HDR")
-    # plt.imsave("dump/"+"Main"+"_"+str(round(meanFLIPError, 6))+".png", flipErrorMap)
+    flipErrorMap, meanFLIPError, parameters = flip.evaluate(ref, test, "HDR")
+    plt.imsave("dump/"+"Main"+"_"+str(round(meanFLIPError, 6))+".png", flipErrorMap)
     #files = glob.glob ("dump/*.jpeg")
         
     #for myFile in files:
@@ -28,15 +28,15 @@ if __name__ == '__main__':
     #    
     
 
-    reference_files = glob.glob("dump/ref/*.png")
-    test_files = glob.glob("dump/test/*.png")
+    # reference_files = glob.glob("dump/ref/*.png")
+    # test_files = glob.glob("dump/test/*.png")
 
-    for ref in reference_files:
-        for test in test_files:
-            test_string = test.split("test")[1].split("\\")[1].split("GI-1.1")[0].strip("_")
-            if ref.__contains__(test_string):
-                flipErrorMap, meanFLIPError, parameters = flip.evaluate(ref, test, "LDR")
-                plt.imsave("dump/results/"+test_string+"_"+str(round(meanFLIPError, 6))+".png", flipErrorMap)
+    # for ref in reference_files:
+    #     for test in test_files:
+    #         test_string = test.split("test")[1].split("\\")[1].split("GI-1.1")[0].strip("_")
+    #         if ref.__contains__(test_string):
+    #             flipErrorMap, meanFLIPError, parameters = flip.evaluate(ref, test, "LDR")
+    #             plt.imsave("dump/results/"+test_string+"_"+str(round(meanFLIPError, 6))+".png", flipErrorMap)
 
     
     
